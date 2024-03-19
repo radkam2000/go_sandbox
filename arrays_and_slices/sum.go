1ed2365398454705c42af6e550f7e4abb3990694
radkam2000 <radkam2000@protonmail.com>
2024-03-19 20:50:52 +0100

    arrays_and_slices exercise done

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -7,3 +7,24 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var tails []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			tails = append(tails, 0)
+		} else {
+			tail := numbers[1:]
+			tails = append(tails, Sum(tail))
+		}
+	}
+	return tails
+}
